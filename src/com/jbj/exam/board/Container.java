f75b7e7f23fd7fa517641b969f9c413134fc567d
bujin21 <cvj0810@naver.com>
2022-10-02 16:31:16 +0900

    기본적인 명령어 처리 기반 마련

---
+++ src/com/jbj/exam/board/Container.java
@@ -0,0 +1,11 @@
+package com.jbj.exam.board;
+
+import java.util.Scanner;
+
+public class Container {
+  public static Scanner scanner;
+
+  static {
+    scanner = new Scanner(System.in);
+  }
+}
--- src/com/jbj/exam/board/Main.java
+++ src/com/jbj/exam/board/Main.java
@@ -1,5 +1,7 @@
+package com.jbj.exam.board;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    new App().run();
   }
 }
